4e1dde3dc20710623e5b5e53f0f1b51c501fd295
sarunas <ssliteris@exadel.com>
2021-12-15 13:25:44 +0200

    feat/init

---
+++ pages/check/7.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div></div>
+    {{ msg }}
+  </div>
+</template>
+
+<script setup>
+  const msg = 'HELLO 7'
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>Hello</div>
+    <div>
+      <NuxtLink to="/check/7">TEST 7</NuxtLink>
+    </div>
+  </div>
+</template>
