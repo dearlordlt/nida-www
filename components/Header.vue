fcf67202286a5bcfd78ad83c18c8d55070442dfa
sarunas <ssliteris@exadel.com>
2021-12-15 20:40:51 +0200

    feat/menu

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-slate-700">
-    <NuxtLink to="/check/7">TEST 7</NuxtLink>
-    <NuxtLink to="/">Back</NuxtLink>
+  <div class="bg-slate-800 p-3">
+    <NuxtLink class="bg-gray-600 rounded-md p-1 m-1" to="/">HOME</NuxtLink>
+    <NuxtLink class="bg-gray-600 rounded-md p-1 m-1" to="/character">CHARACTER</NuxtLink>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <div>Hello</div>
+    <p class="text-lg">Resources:</p>
+    <p>MAP: <a target="blank" href="https://drive.google.com/file/d/1hXeIjkw08pBHdlsU0mDMiTlR-zLdhgPL/view?usp=sharing">link</a></p>
   </div>
 </template>
